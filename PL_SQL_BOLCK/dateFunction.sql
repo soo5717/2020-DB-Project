5c242157eadba188c492b2f772cd10306fdbed83
potatochips178 <57126990+potatochips178@users.noreply.github.com>
2020-06-13 16:41:08 +0900

    Update dateFunction.sql

--- PL_SQL_BOLCK/dateFunction.sql
+++ PL_SQL_BOLCK/dateFunction.sql
@@ -9,9 +9,17 @@ END;
 CREATE OR REPLACE FUNCTION Date2EnrollSemester(dDate in DATE)
 RETURN number
 IS
+  now_month number;
   now_semester number;
 BEGIN
-  now_semester := TO_NUMBER(dDate, 'MM');
+  now_month := TO_NUMBER(dDate, 'MM');
   --1, 2 semester 
+  IF (now_month >=11 and now_month <=12 ) THEN
+    now_semester := 1;
+  ELSE IF (now_month >=1 and now_month <= 4) THEN
+    now_semester := 1;
+  ELSE
+    now_semester:= 2;
+  END IF;
 END;
 /
