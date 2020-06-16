38ab4dc3e0ecb3689877f609ce3c227130025999
suyeon <koo5717@naver.com>
2020-06-16 19:25:51 +0900

    verify enroll

--- TEST_SQL/verifyEnroll.sql
+++ TEST_SQL/verifyEnroll.sql
@@ -23,14 +23,11 @@ CREATE OR REPLACE FUNCTION SelectTimeTable(
     PIPELINED
 IS
     v_enroll enroll_type;
-    cnt_credit NUMBER:= 0;
-    cnt_subject NUMBER := 0;
-    course_time VARCHAR2(70);
 
     CURSOR time_table(v_s_id NUMBER, v_year NUMBER, v_semester NUMBER) IS
         SELECT s.subject_id, s.subject_name, c.course_division, d.department_name,
-            s.subject_group, s.subject_credit, p.professor_name,
-            c.course_start1, c.course_end1, 
+            s.subject_group, s.subject_credit, p.professor_name, c.course_room
+            c.course_start1, c.course_end1,
             NVL(c.course_start2, 00000) course_start2, NVL(c.course_end2, 00000) course_end2
         FROM ENROLL e, COURSES c, SUBJECTS s, DEPARTMENTS d, PROFESSORS p
         WHERE e.student_id = v_s_id
@@ -43,14 +40,9 @@ IS
             AND s.department_id = d.department_id;
 BEGIN
     FOR t IN time_table(sStudentId, nYear, nSemester) LOOP
-        DBMS_OUTPUT.PUT_LINE(t.subject_id || '|' || t.subject_name || '|' 
-                            || t.course_division || '|' || t.department_name || '|' 
-                            || t.subject_group || '|' || t.subject_credit || '|' 
-                            || t.professor_name);
-
-        cnt_subject := cnt_subject + 1;
-        cnt_credit := cnt_credit + t.subject_credit;
-        course_time := '10시 30분';
+        -- 시간 변환 부분 
+        course_time := Number2TableTime(t.course_start1, t.course_end1, 
+                                        t.course_start2, t.course_end2, t.course_room);
 
         v_enroll := enroll_type(
                                     t.subject_id, t.subject_name, 
@@ -60,7 +52,6 @@ BEGIN
                                 );
         PIPE ROW(v_enroll);
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE('총 신청 과목 수 : ' || cnt_subject || ' 총 학점 : ' || cnt_credit);
     RETURN;   
 END;
 /
