cdb829d1fc11101cc822443c0d461589001e3ea0
potatochips178 <57126990+potatochips178@users.noreply.github.com>
2020-06-16 12:00:28 +0900

    Update enroll.sql

--- CREATE_TABLE/enroll.sql
+++ CREATE_TABLE/enroll.sql
@@ -4,7 +4,11 @@ create table ENROLL(
   student_id number not null, 
   enroll_year number not null,
   enroll_semester number not null,
+
   CONSTRAINT enroll_pk PRIMARY KEY (subject_id, student_id), 
   CONSTRAINT enroll_fk_std FOREIGN KEY (student_id) REFERENCES students(student_id), 
   CONSTRAINT enroll_fk_sbj FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
 );
+
+--시간표 조회를 위한 insert문
+insert into enroll values(1014, 01, 1700000, 2020, 1);
