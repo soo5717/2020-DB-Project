23840af6f4cf2e402d3814feeb0a2feb2e2f7348
hyunJIN7 <yunjingong12@naver.com>
2020-06-16 17:31:44 +0900

    number2timatable 숫자처리함수사용

--- PL_SQL_BOLCK/dateFunction.sql
+++ PL_SQL_BOLCK/dateFunction.sql
@@ -26,45 +26,48 @@ RETURN now_semester;
 END;
 /
 
-                                 CREATE OR REPLACE FUNCTION Number2TableTime(
+CREATE OR REPLACE FUNCTION Number2TableTime(
 c_start in number,
 c_end in number,
 c_start2 in number,
-c_end2 in number,
-c_room in VARHCAR2(30)
+c_end2 in number, 
+c_room in VARCHAR2
 )
-RETURN varchar2(70)
+RETURN varchar2
 IS
-  res varchar2(70);
+  res varchar2(150);
   day NUMBER;
   t NUMBER;
-  TYPE time_arr is table of number index bt binary_integer;
+  TYPE time_arr is table of number index by binary_integer;
+  TYPE day_arr is table of varchar2(3) index by binary_integer;
   t_arr time_arr;
-  i number := 2;
+  i number := 1;
+  d_arr day_arr;
+
 BEGIN
 	t_arr(1) := c_start;
-	t_arr(2) = c_end;
-	t_arr(3) = c_start2;
-	t_arr(4) = c_end2;
+	t_arr(2) := c_end;
+	t_arr(3) := c_start2;
+	t_arr(4) := c_end2;
+	d_arr(1) :=  '일 ';
+	d_arr(2) :=  '월 ';
+	d_arr(3) :=  '화 ';
+	d_arr(4) :=  '수 ';
+	d_arr(5) :=  '목 ';
+	d_arr(6) :=  '금 ';
+	d_arr(7) :=  '토 ';
+	
   	res := '';
   	WHILE i<4 loop
-  	day := t_arr(i) / 10000 ;
-	  	CASE day
-	  	WHEN 2 THEN res := res + '월 ';
-	  	WHEN 3 THEN res := res + '화 ';
-	  	WHEN 4 THEN res := res + '수 ';
-	  	WHEN 5 THEN res := res + '목 ';
-	  	WHEN 6 THEN res := res + '금 ';
-	  	WHEN 7 THEN res := res + '토 ';
-	  	WHEN 1 THEN res := res + '일 ';
-	  	END;
-	  	if (day != 0) then
-	  	res := res + to_char( (t_arr(i)%10000)/100 ) + ':' to_char( t_arr(i)%100 ) +'-'  	
-	  				+ to_char( (t_arr(i+1)%10000)/100 ) + ':' to_char( t_arr(i+1)%100 ) +'('+c_room+')';
-  		end if;
+	  	day := t_arr(i) / 10000 ;
+	  	IF day != 0 THEN
+			res := res + d_arr( day );
+			res := res + to_char((t_arr(i) - (t_arr(i) / 10000) * 10000 )/100 ) + ':' + to_char(t_arr(i) - ( t_arr(i) / 1000)*1000 ) +'-' ;	
+			res := res + to_char((t_arr(i+1)-(t_arr(i+1) / 10000) * 10000 ) / 100 ) + ':' + to_char( t_arr(i+1) - ( t_arr(i+1) / 1000)*1000) + '(' + c_room + ')';
+		END IF;
+	  	i := i+2;
   	end loop;
   		
   RETURN res;
 END;
-/
-                                 
+/                     
