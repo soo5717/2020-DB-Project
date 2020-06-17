056b6dfe623aea18416732e94d4decbdf631ef1d
suyeon <koo5717@naver.com>
2020-06-17 19:49:34 +0900

    verifyEnroll out modify

--- PL_SQL_BOLCK/verifyEnroll.sql
+++ PL_SQL_BOLCK/verifyEnroll.sql
@@ -1,17 +1,21 @@
--- OUT 변경될 가능성 있음
+-- 수강조회 프로시저
+-- 총 신청 과목수, 총 학점, 최대 수강학점을 return
 CREATE OR REPLACE PROCEDURE Select2TimeTable(
     sStudentId IN NUMBER,
     nYear IN NUMBER,
     nSemester IN NUMBER,
     cnt_credit OUT NUMBER,
-    cnt_subject OUT NUMBER
+    cnt_subject OUT NUMBER,
+    s_credit OUT NUMBER
 )
 IS
+    course_time VARCHAR2(70);
+
     CURSOR time_table(v_s_id NUMBER, v_year NUMBER, v_semester NUMBER) IS
         SELECT s.subject_id, s.subject_name, c.course_division, d.department_name,
-            s.subject_group, s.subject_credit, p.professor_name,
-            c.course_start1, c.course_end1, 
-            NVL(c.course_start2, 00000), NVL(c.course_end2, 00000)
+            s.subject_group, s.subject_credit, p.professor_name, c.course_room,
+            c.course_start1, c.course_end1,
+            NVL(c.course_start2, 00000) course_start2, NVL(c.course_end2, 00000) course_end2
         FROM ENROLL e, COURSES c, SUBJECTS s, DEPARTMENTS d, PROFESSORS p
         WHERE e.student_id = v_s_id
             AND e.enroll_year = v_year
@@ -26,15 +30,26 @@ BEGIN
     cnt_subject := 0;
     
     FOR t IN time_table(sStudentId, nYear, nSemester) LOOP
-        DBMS_OUTPUT.PUT_LINE(t.subject_id || '|' || t.subject_name || '|' 
-                            || t.course_division || '|' || t.department_name || '|' 
-                            || t.subject_group || '|' || t.subject_credit || '|' 
-                            || t.professor_name);
-
+        -- 시간 변환 부분 
+        course_time := Number2TableTime(t.course_start1, t.course_end1, 
+                                        t.course_start2, t.course_end2, t.course_room);
+        -- 시간표 정보 출력 부분
+        DBMS_OUTPUT.PUT_LINE(t.subject_id || ' | ' || t.subject_name || ' | ' 
+                            || t.course_division || ' | ' || t.department_name || ' | ' 
+                            || t.subject_group || ' | ' || t.subject_credit || ' | ' 
+                            || t.professor_name || ' | ' || course_time);
+        
         cnt_subject := cnt_subject + 1;
         cnt_credit := cnt_credit + t.subject_credit;
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE('총 신청 과목 수 : ' || cnt_subject || ' 총 학점 : ' || cnt_credit);
+
+    SELECT student_credit
+    INTO s_credit
+    FROM STUDENTS
+    WHERE student_id = sStudentId;
+
+    -- 총 신청 과목수, 총 학점 출력 부분
+    DBMS_OUTPUT.PUT_LINE('총 신청 과목수: ' || cnt_subject || ' 총 학점: ' || cnt_credit);
 EXCEPTION
 	WHEN OTHERS THEN
 		DBMS_OUTPUT.PUT_LINE(SQLERRM || '에러 발생');
