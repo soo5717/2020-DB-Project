486a8e3ca5de2e33feda0b8294e6a9aed39c4b77
hyunJIN7 <35680342+hyunJIN7@users.noreply.github.com>
2020-06-17 13:21:35 +0900

    Update dateFunction.sql

--- PL_SQL_BOLCK/dateFunction.sql
+++ PL_SQL_BOLCK/dateFunction.sql
@@ -30,7 +30,7 @@ BEGIN
 RETURN now_semester;
 END;
 /
-
+/*substr 버전*/
 CREATE OR REPLACE FUNCTION Number2TableTime(
 c_start in number,
 c_end in number,
@@ -43,36 +43,41 @@ IS
   res varchar2(150);
   day NUMBER;
   t NUMBER;
-  TYPE time_arr is table of number index by binary_integer;
+  TYPE time_arr is table of varchar2(6) index by binary_integer;
   TYPE day_arr is table of varchar2(3) index by binary_integer;
   t_arr time_arr;
   i number := 1;
   d_arr day_arr;
 
 BEGIN
-	t_arr(1) := c_start;
-	t_arr(2) := c_end;
-	t_arr(3) := c_start2;
-	t_arr(4) := c_end2;
-	d_arr(1) :=  '일 ';
-	d_arr(2) :=  '월 ';
-	d_arr(3) :=  '화 ';
-	d_arr(4) :=  '수 ';
-	d_arr(5) :=  '목 ';
-	d_arr(6) :=  '금 ';
-	d_arr(7) :=  '토 ';
+	t_arr(1) := to_char(c_start);
+	t_arr(2) := to_char(c_end);
+	t_arr(3) := to_char(c_start2);
+	t_arr(4) := to_char(c_end2);
+	d_arr(1) :=  ' 일';
+	d_arr(2) :=  ' 월';
+	d_arr(3) :=  ' 화';
+	d_arr(4) :=  ' 수';
+	d_arr(5) :=  ' 목';
+	d_arr(6) :=  ' 금';
+	d_arr(7) :=  ' 토';
 	
-  	res := '';
+	res := '';
+	
+
   	WHILE i<4 loop
-	  	day := t_arr(i) / 10000 ;
+	  	day := to_number(t_arr(i)) / 10000 ;
+	  	
 	  	IF day != 0 THEN
-			res := res + d_arr( day );
-			res := res + to_char((t_arr(i) - (t_arr(i) / 10000) * 10000 )/100 ) + ':' + to_char(t_arr(i) - ( t_arr(i) / 1000)*1000 ) +'-' ;	
-			res := res + to_char((t_arr(i+1)-(t_arr(i+1) / 10000) * 10000 ) / 100 ) + ':' + to_char( t_arr(i+1) - ( t_arr(i+1) / 1000)*1000) + '(' + c_room + ')';
+			res := res || d_arr( day );
+			
+			res := res || substr(t_arr(i),2,2)||':'  || substr(t_arr(i),4) ||'-' ;
+			res := res || substr(t_arr(i+1),2,2) ||':'  || substr(t_arr(i+1),4);
 		END IF;
 	  	i := i+2;
   	end loop;
-  		
+  res := res ||	'(' || c_room ||')';	
   RETURN res;
 END;
-/                     
+/ 
+                 
