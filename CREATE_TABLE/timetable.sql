17e599fafbcaa75b2d951010e1b6bb1b9f98facb
potatochips178 <potatochips@sookmyung.ac.kr>
2020-06-17 20:34:14 +0900

    Update timetable.sql

--- CREATE_TABLE/timetable.sql
+++ CREATE_TABLE/timetable.sql
@@ -7,17 +7,17 @@ SELECT
     p.professor_name,
     c.course_room,
     c.course_start1,
-    c.course_start2,
+    NVL(c.course_start2, 0),
     c.course_end1,
-    c.course_end2,
+    NVL(c.course_end2, 0)
 FROM
     enroll e, 
     subjects s,
     departments d,
-    profssors p
+    professors p
 WHERE
     e.subject_id = s.subject_id
     AND e.subject_id = c.subject_id
-    AND e.course_division = c.c.course_division
+    AND e.course_division = c.course_division
     AND c.professor_id = p.professor_id
 ;
