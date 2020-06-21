2579771dc23511aeb6c6bf598347d5282ce6a97b
suyeon <koo5717@naver.com>
2020-06-21 17:11:06 +0900

    create table

--- CREATE_TABLE/enroll.sql
+++ CREATE_TABLE/enroll.sql
@@ -18,7 +18,6 @@ insert into enroll values(1006, 01, 1700000, 2020, 1);
 insert into enroll values(1848, 01, 1700001, 2020, 1);
 
 -- enroll 테이블 test용 INSERT
--- 교과과목 번호, 분반, 학번, 신청년도, 신청학기
 INSERT INTO ENROLL VALUES (1018, 1, 1812357, 2020, 1);
 INSERT INTO ENROLL VALUES (1846, 1, 1812357, 2020, 1);
 INSERT INTO ENROLL VALUES (1007, 1, 1812357, 2020, 1);
--- CREATE_TABLE/students.sql
+++ CREATE_TABLE/students.sql
@@ -21,15 +21,7 @@ INSERT INTO students VALUES (1810003, '0000', '최예원', 5, 19, 4, '대구광�
 INSERT INTO students VALUES (1914334, '0000', '김빛나리', 3, 19, 4, '서울특별시 도봉구');
 INSERT INTO students VALUES (1938224, '0000', '박개나리', 2, 19, 5, '부산광역시 해운대구');
 INSERT INTO students VALUES (1901378, '0000', '최봄날', 2, 19, 5, '대전광역시 서구');
+
 -- insertEnrollTest 에서 사용할 데이터
 INSERT INTO students VALUES (1812345, '0000', '예처리', 4, 19, 2, '서울특별시 강남구');
-
--- Update rows in a Table
-
-Update students
-  SET student_address=null
-WHERE student_id=1901378;
-
---확인을 위한 부분
---INSERT INTO students VALUES (2020220, '0000', '테스트', 2, 19, 5, '외계행성');
 
